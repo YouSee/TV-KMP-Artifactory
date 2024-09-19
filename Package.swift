313901aff2d246289c29d1f505477147f79ed268
nicolai obel christensen <nicolaioc@hotmail.dk>
2024-09-19 15:33:58 +0200

    asd

--- Package.swift
+++ Package.swift
@@ -1,24 +1,33 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-   name: "mixpanelLib",
-   platforms: [
-     .iOS(.v14),
-   ],
-   products: [
-      .library(name: "mixpanelLib", targets: ["mixpanelLib"])
-   ],
-   dependencies: [
-         .package(
-             url: "https://github.com/mixpanel/mixpanel-iphone",
-             from: "2.8.0"
-         )
-     ],
-   targets: [
-      .binaryTarget(
-         name: "mixpanelLib",
-         url: "https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
-         checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619")
-   ]
+    name: "KMP-mixpanel",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "KMP-mixpanel",
+            targets: ["KMP-mixpanel", "mixpanelLib"]),
+    ],
+    dependencies: [
+             .package(
+                 url: "https://github.com/mixpanel/mixpanel-iphone",
+                 from: "2.8.0"
+             )
+         ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+                 name: "mixpanelLib",
+                 url: "https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
+                 checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619"),
+        .target(
+            name: "KMP-mixpanel"),
+        .testTarget(
+            name: "KMP-mixpanelTests",
+            dependencies: ["KMP-mixpanel"]),
+    ]
 )
---
+++ Tests/KMP-mixpanelTests/KMP_mixpanelTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import KMP_mixpanel
+
+final class KMP_mixpanelTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
