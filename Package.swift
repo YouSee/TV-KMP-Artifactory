4c3cac7c37a3be40d713b16c7fd7f81adddf396c
nicolai obel christensen <nicolaioc@hotmail.dk>
2024-09-20 14:31:24 +0200

    ASD

--- Package.swift
+++ Package.swift
@@ -4,25 +4,35 @@
 import PackageDescription
 
 let package = Package(
-    name: "mixpanelLib",
+    name: "TestSDKFrameworkWrapper",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "mixpanelLib",
-            targets: ["mixpanelLib"]),
+            name: "TestSDKFrameworkWrapper",
+            targets: ["TestSDKFrameworkWrapper"]),
     ],
     dependencies: [
              .package(
+                name:"Mixpanel",
                  url: "https://github.com/mixpanel/mixpanel-iphone",
                  from: "2.8.0"
              )
          ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+                    name: "TestSDKFrameworkWrapper",         // <--- new wrapper
+                    dependencies: [
+                        .target(name: "mixpanelLib"),
+                        .product(name: "Mixpanel", package: "Mixpanel")// <-- reference the actual binary target here
+                    ],
+                    path: "TestSDKFrameworkWrapper",
+                    publicHeadersPath: ""
+                ),
         .binaryTarget(
                  name: "mixpanelLib",
                  url: "https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
                  checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619")
     ]
 )
+
+
