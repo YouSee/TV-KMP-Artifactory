0f059b13819edc2ca963cd874424a1af5d7f6587
nicolai obel christensen <nicolaioc@hotmail.dk>
2024-09-25 14:01:10 +0200

    adding wrapper target

--- Package.swift
+++ Package.swift
@@ -3,13 +3,13 @@
 
 import PackageDescription
 
+// swift-tools-version:5.3
 let package = Package(
-    name: "TestSDKFrameworkWrapper",
+    name: "MixpanelLibWrapper",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "TestSDKFrameworkWrapper",
-            targets: ["TestSDKFrameworkWrapper"]),
+            name: "MixpanelLibWrapper",
+            targets: ["MixpanelLibWrapper"]),
     ],
     dependencies: [
              .package(
@@ -19,19 +19,15 @@ let package = Package(
              )
          ],
     targets: [
-        .target(
-                    name: "TestSDKFrameworkWrapper",         // <--- new wrapper
-                    dependencies: [
-                        .target(name: "mixpanelLib"),
-                        .product(name: "Mixpanel", package: "Mixpanel")// <-- reference the actual binary target here
-                    ],
-                    path: "TestSDKFrameworkWrapper",
-                    publicHeadersPath: ""
-                ),
+        .target(name: "MixpanelLibWrapper",
+                dependencies: ["Mixpanel", "mixpanelLib"]
+               ),
         .binaryTarget(
                  name: "mixpanelLib",
                  url: "https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
-                 checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619")
+                 checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619"),
+        
+    
     ]
 )
 
