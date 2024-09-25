5b7986b4a511100e69506423774b07e0e03357c3
nicolai obel christensen <nicolaioc@hotmail.dk>
2024-09-25 14:10:34 +0200

    qwe

--- Package.swift
+++ Package.swift
@@ -13,21 +13,19 @@ let package = Package(
     ],
     dependencies: [
              .package(
-                name:"Mixpanel",
                  url: "https://github.com/mixpanel/mixpanel-iphone",
                  from: "2.8.0"
              )
          ],
     targets: [
-        .target(name: "MixpanelLibWrapper",
-                dependencies: ["Mixpanel", "mixpanelLib"]
-               ),
         .binaryTarget(
                  name: "mixpanelLib",
-                 url: "https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
+                 url:"https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
                  checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619"),
-        
-    
+        .target(name: "MixpanelLibWrapper",
+                    dependencies: ["Mixpanel", "mixpanelLib"],
+                    path: "Sources/MixpanelLibWrapper"
+                   )
     ]
 )
 
