a8dca38cb54a62652e6d6836fdfe32167645cb1e
nicolai obel christensen <nicolaioc@hotmail.dk>
2024-09-27 14:09:50 +0200

    df

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
                  name: "mixpanelLib",
                  url:"https://github.com/YouSee/TV-KMP-Artifactory/raw/refs/heads/main/mixpanelLib.xcframework.zip",
-                 checksum:"da45ba9671dfc104b6d2a3d4dc7215c937d77bdcdcddb9f8e765b94940ec0619"),
+                 checksum:"ee5fed1da01690f51b3116bd6b21e0b101a7fd487b3bd5a96bc4c00b174a8cbe"),
         .target(
             name: "MixpanelLibWrapper",
             dependencies: [
